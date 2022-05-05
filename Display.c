afc7b6d6153a2b82fe159d9c8e3862dd35628eef
Darko Draskovic <darko.draskovic@gmail.com>
2022-05-05 17:59:41 +0200

    Add render texture pass to renderer
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- Display.c
+++ Display.c
@@ -6,30 +6,38 @@
 #include "Hooks.h"
 #include "raylib.h"
 
-/* Image image; */
+// framebuffer
 unsigned int* drawBuffer;
 unsigned int* clearBuffer;
-Texture2D texture;
-int width;
-int scale;
+Texture2D bufferTexture;
 int bufferSize;
 
+// render texture
+RenderTexture2D renderTexture;
+
+int width;
+int height;
+int scale;
+
 void InitRenderer(DisplayConfig displayConfig) {
-  InitWindow(displayConfig.width * displayConfig.scale,
-             displayConfig.height * displayConfig.scale,
+  InitWindow(displayConfig.width * displayConfig.scale, displayConfig.height * displayConfig.scale,
              displayConfig.windowTitle);
 
   SetTargetFPS(60);
 
+  renderTexture = LoadRenderTexture(displayConfig.width, displayConfig.height);
+
   Image image = GenImageColor(displayConfig.width, displayConfig.height, BLACK);
   drawBuffer = image.data;
-  image = GenImageColor(displayConfig.width, displayConfig.height, BLACK);
+  // (Color){0} is transparent: draw render texture over buffer texture
+  image = GenImageColor(displayConfig.width, displayConfig.height, (Color){0});
   clearBuffer = image.data;
-  texture = LoadTextureFromImage(image);
+  bufferTexture = LoadTextureFromImage(image);
+  bufferSize = displayConfig.width * displayConfig.height * sizeof(unsigned int);
 
   width = displayConfig.width;
+  height = displayConfig.height;
   scale = displayConfig.scale;
-  bufferSize = width * displayConfig.height * sizeof(unsigned int);
 }
 
 // TODO: handle alpha
@@ -45,22 +53,17 @@ inline Color ColorScale(Color color, float s) {
 inline Color ColorMix(Color color1, Color color2, float ratio) {
   color1 = ColorScale(color1, 1 - ratio);
   color2 = ColorScale(color2, ratio);
-  return (Color){color1.r + color2.r, color1.g + color2.g, color1.b + color2.b,
-                 255};
+  return (Color){color1.r + color2.r, color1.g + color2.g, color1.b + color2.b, 255};
 }
 
 inline unsigned int ColorToUnsignedInt(Color color) {
-  return (((unsigned int)color.a << 24) | ((unsigned int)color.b << 16) |
-          ((unsigned int)color.g << 8) | (unsigned int)color.r);
+  return (((unsigned int)color.a << 24) | ((unsigned int)color.b << 16) | ((unsigned int)color.g << 8) |
+          (unsigned int)color.r);
 }
 
-void SetPixel(unsigned int x, unsigned int y, unsigned int color) {
-  *(drawBuffer + y * width + x) = color;
-}
+void SetPixel(unsigned int x, unsigned int y, unsigned int color) { *(drawBuffer + y * width + x) = color; }
 
-static inline void clearBackground() {
-  memcpy(drawBuffer, clearBuffer, bufferSize);
-}
+static inline void ClearBuffer() { memcpy(drawBuffer, clearBuffer, bufferSize); }
 
 int Run(DisplayConfig displayConfig) {
   InitRenderer(displayConfig);
@@ -70,22 +73,31 @@ int Run(DisplayConfig displayConfig) {
     Update();      // hook
     PostUpdate();  // hook
 
-    BeginDrawing();
+    BeginTextureMode(renderTexture);
 
-    clearBackground();
-    // set buffer values
+    ClearBackground(BLACK);
+    ClearBuffer();
+
     Draw();  // hook
 
-    // draw buffer
-    UpdateTexture(texture, drawBuffer);
-    DrawTextureEx(texture, (Vector2){0, 0}, 0, scale, WHITE);
+    EndTextureMode();
+
+    BeginDrawing();
+
+    DrawTexturePro(renderTexture.texture, (Rectangle){0, 0, width, -height},
+                   (Rectangle){0, 0, width * scale, height * scale}, (Vector2){0, 0}, 0, WHITE);
+
+    UpdateTexture(bufferTexture, drawBuffer);
+    DrawTextureEx(bufferTexture, (Vector2){0, 0}, 0, scale, WHITE);
 
     PostDraw();  // hook
 
     EndDrawing();
   }
 
-  Close();
+  Close();  // hook
+
+  UnloadRenderTexture(renderTexture);
   CloseWindow();  // Close window and OpenGL context
 
   return 0;
